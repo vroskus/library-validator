241a737fea938449545112821fa9970b5a5bf381
Vilius Roškus <vro@danskebank.lt>
2024-12-26 11:25:53 +0200

    Update

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,6 @@ import type {
   AnySchema,
 } from 'joi';
 
-/* eslint-disable perfectionist/sort-modules */
-
 export type $Validator = AnySchema;
 
 export type $Schema = (...args: unknown[]) => $Validator;
